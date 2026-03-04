580f90f061aabbaddf18ebe5b4eb57da376146b1
Quan1911 <Maianhquan1911@gmail.com>
2026-03-04 19:08:19 +0700

    Update ReadingDAO.java
    Mục tiêu: Chuyển đổi dữ liệu từ đối tượng Java (DTO) vào bảng trong SQL Server.
    Sơ đồ luồng:
    ReadingDTO → ReadingDAO → SQL Query (?) → JDBC → SQL Server
     
    Ý là Hàm này add đối tượng ReadingDTO mới và lưu nó vào bảng Reading trong SQL

--- AQSystem/src/java/model/ReadingDAO.java
+++ AQSystem/src/java/model/ReadingDAO.java
@@ -1,13 +1,34 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package model;
 
-/**
- *
- * @author Admin
- */
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.Timestamp;
+import utils.DBUtils;
+
 public class ReadingDAO {
     
+    public boolean insertReading(ReadingDTO reading) {
+        String sql = "INSERT INTO Reading (sensor_id, pollutant_id, ts, value, quality_flag)"
+                   + " VALUES (?, ?, ?, ?, ?)";
+        
+        try (Connection conn = DBUtils.getConnection()) { 
+            if (conn != null) {
+                PreparedStatement ps = conn.prepareStatement(sql);
+
+                ps.setInt(1, reading.getSensorID());
+                ps.setInt(2, reading.getPollutantID());
+                // Chuyển LocalDateTime của Java sang Timestamp của SQL
+                ps.setTimestamp(3, Timestamp.valueOf(reading.getTs())); 
+                ps.setDouble(4, reading.getValue());
+                ps.setString(5, reading.getQualityFlag());
+                
+                int row = ps.executeUpdate(); 
+                
+                return row > 0; 
+            }
+        } catch (Exception e) {
+            e.printStackTrace(); 
+        }
+        return false; 
+    }
 }
